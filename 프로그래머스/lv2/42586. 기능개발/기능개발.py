5c0899d90b1e7f626daf66d6279324d4e11916ad
주영 <103028061+juyeong525@users.noreply.github.com>
2023-05-22 16:47:09 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,7 +1,6 @@
 import math
 def solution(progresses, speeds):
     count = 0
-    works = []
     n = []
     a = math.ceil((100 - progresses[0]) / speeds[0])
     for i in range(len(progresses)):
@@ -11,7 +10,5 @@ def solution(progresses, speeds):
             a = math.ceil((100 - progresses[i]) / speeds[i])
             n.append(count)
             count = 1
-        works.append(math.ceil((100 - progresses[i]) / speeds[i]))
-    print(works)
     n.append(count)
     return n
