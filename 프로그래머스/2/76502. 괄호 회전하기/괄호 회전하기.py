f5bdc88057c6c115a52fe59a4e00be9980c6262f
주영 <103028061+juyeong525@users.noreply.github.com>
2023-10-25 20:51:15 +0900

    Update 괄호 회전하기.py

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,6 +1,5 @@
 def solution(s):
     index = 0
-    s.replace("[]", "")
     for i in range(len(s)):
         a = s
         while True:
@@ -12,7 +11,5 @@ def solution(s):
                 if len(a) == 0:
                     index += 1
                 break
-        # if s.find("(") <= s.find(")") and s.count("(") == s.count(")") and s.find("[") <= s.find("]") and s.count("[") == s.count("]") and s.find("{") <= s.find("}") and s.count("{") == s.count("}"):
-        #     index += 1
         s = s[1:] + s[0]
     return index
