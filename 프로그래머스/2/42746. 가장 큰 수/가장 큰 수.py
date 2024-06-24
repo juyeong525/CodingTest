2adcd79fbda18beb0c70f23f3e7df532bd68c66e
주영 <103028061+juyeong525@users.noreply.github.com>
2024-06-24 16:11:02 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,6 @@
-from itertools import combinations, permutations
-
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(reverse=True, key= lambda x: x * 4)
+    numbers = list(map(str, numbers)) # join 사용하기 위해 str으로 mapping
+    numbers.sort(reverse=True, key= lambda x: x * 5) # 4을 곱해줌으로써 1의 자리수 
     if "".join(numbers).replace("0","") == "":
         return "0"
     else:
