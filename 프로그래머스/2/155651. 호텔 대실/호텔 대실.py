4a73b05f07deed74ddb35f6495adfdf64f19ddfc
주영 <103028061+juyeong525@users.noreply.github.com>
2024-08-08 12:19:05 +0900

    [level 2] Title: 호텔 대실, Time: 39.48 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -1,24 +1,17 @@
 def solution(book_time):
-    book_int_time = []
-    for book in book_time:
-        book_int = []
-        for b in book:
-            book_int.append(toM(b))
-        book_int_time.append(book_int)
+    book_int_time = map(toM, book_time)
     stack = []
     
     for i in sorted(book_int_time):
         stack.sort(reverse=True, key = lambda x: x[1])
-        if len(stack) == 0:
-            stack.append(i)
-        else:
-            if stack[-1][1] + 10 > i[0]:
-                stack.append(i)
-            else:
-                stack.pop()
-                stack.append(i)
+        if len(stack) != 0 and stack[-1][1] + 10 <= i[0] :
+            stack.pop()
+        stack.append(i)
     return len(stack)
 
-def toM(string):
-    h, m = string.split(":")
-    return int(h) * 60 + int(m)
+def toM(l):
+    result = []
+    for i in l:
+        h, m = i.split(":")
+        result.append(int(h) * 60 + int(m))
+    return result
